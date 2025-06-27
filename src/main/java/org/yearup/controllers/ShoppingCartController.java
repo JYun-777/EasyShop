2c4918e04087d738f7a8a1a3b815a99560bea71a
JonY <jonathanwyun@gmail.com>
2025-06-27 04:23:49 -0700

    Initial Shopping cart attempt
    Fails at updating quantity of items.

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,9 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -12,6 +15,10 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
+@RequestMapping("cart")
+@PreAuthorize("hasRole('USER')")
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -19,9 +26,16 @@ public class ShoppingCartController
     private UserDao userDao;
     private ProductDao productDao;
 
-
+    @Autowired
+    public ShoppingCartController(UserDao userDao, ShoppingCartDao shoppingCartDao, ProductDao productDao)
+    {
+        this.userDao = userDao;
+        this.shoppingCartDao = shoppingCartDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
     public ShoppingCart getCart(Principal principal)
     {
         try
@@ -30,10 +44,16 @@ public class ShoppingCartController
             String userName = principal.getName();
             // find database user by userId
             User user = userDao.getByUserName(userName);
+
+            if (user == null) {
+                System.out.println("User not found.");
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            return shoppingCartDao.getByUserId(userId);
         }
         catch(Exception e)
         {
@@ -43,14 +63,66 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("/products/{productId}")
+    public ShoppingCart addProductToCart(@PathVariable int productId, Principal principal){
+        // get the currently logged in username
+        String userName = principal.getName();
+        // find database user by userId
+        User user = userDao.getByUserName(userName);
 
+        if (user == null) {
+            System.out.println("User not found.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        int userId = user.getId();
+
+        shoppingCartDao.addProductToCart(userId, productId);
+
+        return shoppingCartDao.getByUserId(userId);
+    }
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping("/products/{productId}")
+    public ShoppingCart updateProductQuantity(@PathVariable int productId, Principal principal){
+        // get the currently logged in username
+        String userName = principal.getName();
+        // find database user by userId
+        User user = userDao.getByUserName(userName);
 
+        if (user == null) {
+            System.out.println("User not found.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        int userId = user.getId();
+
+        shoppingCartDao.updateProductQuantity(userId, productId);
+
+        return shoppingCartDao.getByUserId(userId);
+    }
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
+    @DeleteMapping
+    public ShoppingCart emptyCart(Principal principal){
+        // get the currently logged in username
+        String userName = principal.getName();
+        // find database user by userId
+        User user = userDao.getByUserName(userName);
+
+        if (user == null) {
+            System.out.println("User not found.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        int userId = user.getId();
+
+        shoppingCartDao.clearCart(userId);
+
+        return shoppingCartDao.getByUserId(userId);
+    }
 
 }
---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,111 @@
+package org.yearup.data.mysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.data.ProductDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    private final ProductDao productDao;
+
+    @Autowired
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao)
+    {
+        super(dataSource);
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId)
+    {
+        ShoppingCart cart = new ShoppingCart();
+
+        String sql = "SELECT * FROM shopping_cart WHERE user_id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, userId);
+
+            ResultSet rs = stmt.executeQuery();
+
+                while(rs.next()) {
+                    int productId = rs.getInt("product_id");
+                    int quantity = rs.getInt("quantity");
+
+                    Product product = productDao.getById(productId);
+
+                    if (product != null) {
+                        ShoppingCartItem item = new ShoppingCartItem();
+                        item.setProduct(product);
+                        item.setQuantity(quantity);
+                        item.setDiscountPercent(BigDecimal.ZERO);
+                        cart.add(item);
+                    }
+                }
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+        return cart;
+    }
+
+
+
+    @Override
+    public void addProductToCart(int userId, int productId){
+        String sql = "INSERT INTO shopping_cart (user_id, product_id, quantity) VALUES (?, ?, 1)";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            stmt.setInt(2, productId);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void updateProductQuantity(int userId, int productId){
+        String sql = "UPDATE shopping_cart SET quantity = quantity + 1 WHERE user_id = ? and product_id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            stmt.setInt(2, productId);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void clearCart(int userId){
+        String sql = "DELETE from shopping_cart WHERE user_id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
